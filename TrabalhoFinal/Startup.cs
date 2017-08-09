8df18982e87e174ea5fdfbcc20f9f5e8361a6142
Isabelle <Isabelle@LAPTOP-CN7VNT6D>
2017-08-08 21:44:47 -0300

    Adicionar arquivos de projeto.

---
+++ TrabalhoFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrabalhoFinal.Startup))]
+namespace TrabalhoFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
